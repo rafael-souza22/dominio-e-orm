6ab8b2c538c2177ee052817fd3c5fd8620a42f3f
Rafael Souza <rafael.gustavo221188@gmail.com>
2023-11-29 10:13:21 -0300

    DevSuperior - Modelo de Dominio e ORM

---
+++ src/main/java/com/rafael/dominio/entities/Categoria.java
@@ -0,0 +1,52 @@
+package com.rafael.dominio.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "categoria")
+	List<Atividade> atividades = new ArrayList<>();
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Atividade> getAtividades() {
+		return atividades;
+	}
+}
